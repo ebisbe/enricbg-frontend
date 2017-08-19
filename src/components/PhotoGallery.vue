56743ba00b665d38ee5018f7ee66e25c29d9f7d3
Enric Bisbe Gil <enricu@gmail.com>
2017-08-19 17:03:15 +0200

    Initial commit

---
+++ src/components/PhotoGallery.vue
@@ -0,0 +1,19 @@
+<template>
+    <a :href="urlPhoto" :class="classAtt"
+       data-position="bottom">
+        <img :src="urlPhoto" :alt="photo.title" :data-position="position"/>
+    </a>
+</template>
+<script>
+  export default {
+    props: ['photo', 'classSpan', 'position'],
+    computed: {
+      urlPhoto: function () {
+        return 'https://farm' + this.photo.farm + '.staticflickr.com/' + this.photo.server + '/' + this.photo.id + '_' + this.photo.secret + '_b.jpg'
+      },
+      classAtt: function () {
+        return 'image filtered ' + this.classSpan
+      }
+    }
+  }
+</script>
